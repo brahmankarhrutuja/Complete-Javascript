2f5c03b6941e39c8e0bd4d2573e9d3606ec7c34d
Hrutuja Rahul Brahmankar <141763509+brahmankarhrutuja@users.noreply.github.com>
2024-02-03 06:57:37 +0000

    practice some extra 5 questions related to 01,02,03

---
+++ 04- PracticeSet.js
@@ -0,0 +1,26 @@
+// Q1- Create a variable of type string and add a number to it.
+console.log("----------Answer1----------");
+let a = "PhoneNo:";
+let b  = 9876543210;
+console.log(a+b);
+
+//Q2- Use typeof operator to find the datatype of the string in last question.
+console.log("----------Answer2----------");
+console.log(typeof(a+b));
+
+//Q3- Create a const object in javascript can you change it to hold a number later.
+console.log("------------Answer3------------");
+console.log(no);
+
+//Q4 - add new key object.
+console.log("------------Answer4------------");
+const a1 = {
+  name : "Hrutuja";
+  Age: 24;
+  Gender: "Female";
+}
+a1['surname'] = "Brahmankar";
+console.log(a1);
+
+//Q5- Write a JS program to create a word meaning dictionary of 5 words.
+console.log("------------Answer5------------");
